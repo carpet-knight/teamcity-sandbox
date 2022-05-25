63ec203dd34a2d92321e0a928c18ccb1ad8652a7
Ivan Vasilev <vanokvas@gmail.com>
2022-05-26 00:35:48 +0300

    One more commit

--- example-test.js
+++ example-test.js
@@ -1,8 +1,17 @@
 const tsm = require('teamcity-service-messages')
 
-tsm.testStarted({name: 'Example Test'})
+tsm.blockOpened({name: 'Example test block'})
 
-tsm.message({text: 'Some info about test'})
-tsm.testStdErr({name: 'Error motherfuckers', out: 'Error message motherfuckers'})
+tsm.testSuiteStarted({name: 'Suite 1'})
 
-tsm.testFinished({name: 'Example Test'})
+tsm.testStarted({name: 'Test 1'})
+tsm.testFinished({name: 'Test 1'})
+
+tsm.testStarted({name: 'Test 2'})
+tsm.testFailed({name: 'Test 2'})
+
+tsm.testSuiteFinished({name: 'Suite 1'})
+
+tsm.buildProblem({description: 'Some shit happened'})
+
+tsm.blockClosed({name: 'Example test block'})
